61022cadc4ee04bfca31748e51357982533d7872
mohammadsariqkhan <mohsariqkhan2004@gmail.com>
2023-07-18 19:23:39 +0530

    first commit

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try{
+        mongoose.connect(process.env.MONGO_URL!);
+        const connection = await mongoose.connection
+        connection.on('connection',() => {
+            console.log('connected');
+            
+        })
+        connection.on('error',(err) => {
+            console.log(`err ${err}`);
+            
+        })
+
+    } catch(err){
+            console.log('somthing wrong');
+            
+    }
+}
+
